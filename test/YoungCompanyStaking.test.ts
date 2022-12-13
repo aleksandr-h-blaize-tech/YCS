faee6f504cd71ea07f166c1d7c97257ab5b31bbd
hnedenko <73238004+hnedenko@users.noreply.github.com>
2022-12-13 14:50:17 +0200

    add contract templates

---
+++ test/YoungCompanyStaking.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Contract: YoungConpanyStaking", () => {
+    it("Should return the new greeting once it's changed", async () => {
+        
+    });
+});
