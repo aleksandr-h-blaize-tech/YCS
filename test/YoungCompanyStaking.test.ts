644797a15f85a516d1aed175ec6092213ab0a3bb
hnedenko <73238004+hnedenko@users.noreply.github.com>
2022-12-26 18:14:31 +0200

    test: some negative cases

--- test/YoungCompanyStaking.test.ts
+++ test/YoungCompanyStaking.test.ts
@@ -3,13 +3,8 @@ import { takeSnapshot, time } from "@nomicfoundation/hardhat-network-helpers";
 
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Contract, constants, utils } from "ethers";
-import { deployContract, MockProvider } from "ethereum-waffle";
 
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { BigNumber } from "@ethersproject/bignumber";
-import type { Event } from "@ethersproject/contracts";
-import { string } from "hardhat/internal/core/params/argumentTypes";
 
 const { AddressZero } = ethers.constants;
 
@@ -34,7 +29,7 @@ describe("Contract: YoungCompanyStaking", function () {
     const week = 7 * day;
 
     // YCS setting
-    let lockTIme = week;
+    let lockTime = week;
     let rewardPercentage = 10;
     
     beforeEach(async () => {
@@ -63,7 +58,7 @@ describe("Contract: YoungCompanyStaking", function () {
             await YCS.deployed();
 
             // Initialize YoungCompanyStaking lockTime and and rewardPercentage
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // Check state
             expect(await YCS.token()).to.be.eq(token.address);
@@ -76,7 +71,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // User1 deposit
             let amountUser1 = ethers.utils.parseEther("100");
@@ -121,7 +116,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // User1 deposit
             let amountUser1 = ethers.utils.parseEther("100");
@@ -135,9 +130,9 @@ describe("Contract: YoungCompanyStaking", function () {
                 rewardPercentage);
 
             // Owner change lockTime
-            let newLockTIme = 2 * week;
-            tx = await YCS.setLockTime(newLockTIme);
-            await expect(tx).to.emit(YCS, "LockTimeChanged").withArgs(lockTIme, newLockTIme);
+            let newlockTime = 2 * week;
+            tx = await YCS.setLockTime(newlockTime);
+            await expect(tx).to.emit(YCS, "LockTimeChanged").withArgs(lockTime, newlockTime);
 
             // Owner change rewardPercentage        
             let newRewardPercentage = 30;
@@ -152,7 +147,7 @@ describe("Contract: YoungCompanyStaking", function () {
                 })            
             await expect(tx).to.emit(YCS, "Deposited").withArgs(
                 users[1].address, amountUser2,
-                await time.latest(), await time.latest() + newLockTIme,
+                await time.latest(), await time.latest() + newlockTime,
                 newRewardPercentage);
 
             // Owner show state
@@ -166,7 +161,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // Owner add Admin
             let tx = await YCS.addAdmin(admin.address);
@@ -208,7 +203,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // Owner add Admin
             let tx = await YCS.addAdmin(admin.address);
@@ -262,7 +257,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // User1 deposit
             let amountUser1 = ethers.utils.parseEther("100");
@@ -281,7 +276,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // User1 deposit
             let amountUser1 = ethers.utils.parseEther("100");
@@ -304,7 +299,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // User1 deposit
             let amountUser1 = ethers.utils.parseEther("100");
@@ -326,7 +321,7 @@ describe("Contract: YoungCompanyStaking", function () {
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             YCS = await ycs.deploy(token.address);
             await YCS.deployed();
-            await YCS.initialize(lockTIme, rewardPercentage);
+            await YCS.initialize(lockTime, rewardPercentage);
 
             // User1 deposit
             let amountUser1 = ethers.utils.parseEther("100");
@@ -354,6 +349,104 @@ describe("Contract: YoungCompanyStaking", function () {
             // Deploy YoungCompanyStaking
             const ycs = await ethers.getContractFactory("YoungCompanyStaking");
             await expect(ycs.deploy(AddressZero)).to.be.reverted;            
+        });
+    });
+
+    describe("Additional test cases: full coverage", function () {
+        it("Initialize method can only be called once", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+            
+            // Initialize YoungCompanyStaking with lockTime and rewardPercentage
+            await YCS.initialize(lockTime, rewardPercentage);
+            
+            // Attempt to initialize YoungCompanyStaking again, should throw an error
+            await expect(YCS.initialize(lockTime, rewardPercentage)).to.be.revertedWith("Initializable: contract is already initialized");
+        });
+
+        it("Initialize: LockTime must be between 0 and 26 weeks", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+          
+            // Attempt to initialize YoungCompanyStaking with lockTime greater than 26 weeks, should throw an error
+            await expect(YCS.initialize(27 * week, rewardPercentage)).to.be.revertedWithCustomError(YCS, "IncorrectLockTime");
+        });
+
+        it("Initialize: RewardPercentage must be between 0 and 50", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+          
+            // Attempt to initialize YoungCompanyStaking with rewardPercentage greater than 50, should throw an error
+            await expect(YCS.initialize(lockTime, 51)).to.be.revertedWithCustomError(YCS, "IncorrectRewardPercentage");
+        });
+
+        it("Initialize method can only be called by the contract owner", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+          
+            // Attempt to initialize YoungCompanyStaking as a non-owner account, should throw an error
+            await expect(YCS.connect(admin).initialize(lockTime, rewardPercentage)).to.be.reverted;
+        });
+
+        it("AddAdmin method can only be called with a valid address", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+            await YCS.initialize(lockTime, rewardPercentage);
+          
+            // Grant admin role to user[0]
+            await YCS.addAdmin(users[0].address);
+          
+            // Attempt to add an admin with a zero address, should throw an error
+            await expect(YCS.connect(owner).addAdmin(AddressZero)).to.be.revertedWithCustomError(YCS, "ZeroAddress");
+        });
+
+        it("AddAdmin method can only be called with an address that is not already an admin", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+            await YCS.initialize(lockTime, rewardPercentage);
+          
+            // Grant admin role to user[0]
+            await YCS.addAdmin(users[0].address);
+          
+            // Attempt to add user[0] as an admin again, should throw an error
+            await expect(YCS.connect(owner).addAdmin(users[0].address)).to.be.revertedWithCustomError(YCS, "AdminAlreadyGranted");
+        });
+
+        it("AddAdmin method emits the correct event", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+            await YCS.initialize(lockTime, rewardPercentage);
+          
+            // Grant admin role to user[0]
+            let tx = await YCS.addAdmin(users[0].address);
+            await expect(tx).to.emit(YCS, "AdminAdded").withArgs(users[0].address);
+          
+            // Add user[1] as an admin
+            tx = await YCS.connect(owner).addAdmin(users[1].address);
+            await expect(tx).to.emit(YCS, "AdminAdded").withArgs(users[1].address);
+          });
+
+        it("AddAdmin method adds the correct role to the correct address", async () => {
+            const ycs = await ethers.getContractFactory("YoungCompanyStaking");
+            YCS = await ycs.deploy(token.address);
+            await YCS.deployed();
+            await YCS.initialize(lockTime, rewardPercentage);
+          
+            // Grant admin role to user[0]
+            await YCS.addAdmin(users[0].address);
+          
+            // Add user[1] as an admin
+            await YCS.connect(owner).addAdmin(users[1].address);
+          
+            // Check that user[1] has the admin role
+            expect(await YCS.hasRole(YCS.ADMIN_ROLE(), users[1].address)).to.be.true;
         });
     });
 
